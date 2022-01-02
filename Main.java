025677f4245d9b66472263e1448724bf9fe65d37
DOUi <doui@doui-pc.localdomain>
2022-01-02 17:20:55 -0500

    Updated OS detection and provided .jar file for convenience

---
+++ Main.java
@@ -0,0 +1,122 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		String CURR_DIR = System.getProperty("user.dir");
+		if (System.getProperty("os.name").equals("Linux")) {
+			File chromeFile = new File(CURR_DIR + "/linux/chromedriver");
+			System.setProperty("webdriver.chrome.driver", chromeFile.getAbsolutePath());
+		} else if (System.getProperty("os.name").equals("Windows")) {
+			File chromeFile = new File(CURR_DIR + "/windows/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", chromeFile.getAbsolutePath());
+		} else {
+			File chromeFile = new File(CURR_DIR + "/mac/chromedriver");
+			System.setProperty("webdriver.chrome.driver", chromeFile.getAbsolutePath());
+		}
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.jklm.fun");
+		driver.manage().window().maximize();
+
+		Path filePath = new File(CURR_DIR + "/wordlist.txt").toPath();
+
+		Charset charset = Charset.defaultCharset();
+
+		List<String> stringList = Files.readAllLines(filePath, charset);
+		String[] wordlist = stringList.toArray(new String[] {});
+
+		// System.out.println ("Bombparty-Bot Started Successfully");
+
+		while (true) {
+			while (true) {
+				try {
+					driver.switchTo().frame(0); // Switch to correct iframe
+				} catch (Exception e) {
+					break;
+				}
+			}
+
+			while (true) {
+				try {
+					driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/div[1]/button")).click(); // Auto join
+																											// game
+				} catch (Exception e) {
+					break;
+				}
+			}
+
+			try {
+				if (!driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[2]/div[1]")).isDisplayed()) {
+					LinkedList<String> listOfWords = new LinkedList<>();
+
+					String syllable = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div"))
+							.getText().toLowerCase();
+
+					System.out.println(syllable);
+
+					WebElement answerBox = driver
+							.findElement(By.xpath("/html/body/div[2]/div[3]/div[2]/div[2]/form/input"));
+
+					for (String line : wordlist) {
+						if (line.contains(syllable)) {
+							listOfWords.add(line);
+						}
+					}
+
+					int random = (int) (Math.random() * (listOfWords.size() - 0) + 0);
+
+					String matchingWord = listOfWords.get(random);
+
+					answerBox.click();
+
+					TimeUnit.SECONDS.sleep((int) (Math.random() * (65 - 15) + 15) / 100);
+
+					for (char character : matchingWord.toCharArray()) {
+						int fail = (int) (Math.random() * (300 - 0) + 0);
+						if (fail == 2) {
+							int loops = (int) (Math.random() * (3 - 0) + 0);
+
+							for (int i = 0; i < loops; i++) {
+								answerBox.sendKeys(Character.toString((char) ('A' + Math.random() * ('Z' - 'A' + 1))));
+								TimeUnit.SECONDS.sleep((int) (Math.random() * (5 - 2) + 2) / 100);
+							}
+
+							TimeUnit.MILLISECONDS.sleep(100);
+
+							for (int i = 0; i < loops; i++) {
+								answerBox.sendKeys(Keys.BACK_SPACE);
+							}
+
+							TimeUnit.MILLISECONDS.sleep(100);
+
+							answerBox.sendKeys(Character.toString(character));
+
+						} else {
+							answerBox.sendKeys(Character.toString(character));
+							TimeUnit.SECONDS.sleep((int) (Math.random() * (14 - 5) + 5) / 100);
+						}
+					}
+					answerBox.sendKeys(Keys.RETURN);
+					TimeUnit.MILLISECONDS.sleep(50);
+
+				}
+			} catch (Exception e) {
+			}
+
+		}
+
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# BombpartyBot
+An improved version of my old python bot, now in java, based off msesjrl's python selenium bot
+
+# How to run
+If you have java installed you should be able to double-click **Bombparty-bot.jar** and it should run.
+
+You can also open a terminal or cmd in the folder and run:
+```
+java -jar Bombparty-bot.jar
+```
+
+If you dont have java you can get it for free at https://www.oracle.com/java/technologies/javase-jdk16-downloads.html
+
+# Errors
+If you run the bot with the terminal and get chromedriver errors then open your google chrome and check what version (9x.xxx...) you have. Then download that version from https://chromedriver.chromium.org/downloads and replace the one in provided in your OS' folder. For example if you have version 92.0.4515.159 you would download the ChromeDriver for version 92.
