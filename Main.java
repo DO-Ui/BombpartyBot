c9fa6f5a7e21f9f65605dfce16f25291d706656a
DO-Ui <68296986+DO-Ui@users.noreply.github.com>
2022-01-19 16:25:31 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
 
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -38,29 +39,22 @@ public class Main {
 		String[] wordlist = stringList.toArray(new String[] {});
 
 		while (true) {
-			if (!(driver.findElements(By.xpath("/html/body/div[2]/div[4]/div[1]/iframe")).size() > 0))
-			while (true) {
+			if ((driver.findElements(By.xpath("/html/body/div[2]/div[4]/div[1]/iframe")).size() > 0)) {
 				try {
 					driver.switchTo().frame(0); // Switch to correct iframe
-					if (!(driver.findElements(By.xpath("/html/body/div[2]/div[3]/div[1]/div[1]/button")).size() > 0)) {
+					if (!(driver.findElements(By.xpath("/html/body/div[2]/div[3]/div[1]/div[1]/button"))
+							.size() > 0)) {
 						driver.switchTo().parentFrame();
 					}
 				} catch (Exception e) {
-					break;
-				}
-			}
-			
-			while (true) {
-				try {
-					driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/div[1]/button")).click(); // Auto join
-																											// game
-				} catch (Exception e) {
-					break;
 				}
 			}
 
 			try {
 				if (!driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[2]/div[1]")).isDisplayed()) {
+					if (driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/div[1]/button")).isDisplayed()) {
+						driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/div[1]/button")).click(); // Auto join
+					}
 					LinkedList<String> listOfWords = new LinkedList<>();
 
 					String syllable = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div"))
@@ -83,19 +77,19 @@ public class Main {
 
 					answerBox.click();
 
-					TimeUnit.SECONDS.sleep((int) (Math.random() * (65 - 15) + 15) / 100);
+					// TimeUnit.SECONDS.sleep((int) (Math.random() * (65 - 15) + 15) / 100);
 
 					for (char character : matchingWord.toCharArray()) {
-						int fail = (int) (Math.random() * (300 - 0) + 0);
+						int fail = (int) (Math.random() * (1000 - 0) + 0);
 						if (fail == 2) {
 							int loops = (int) (Math.random() * (3 - 0) + 0);
 
 							for (int i = 0; i < loops; i++) {
 								answerBox.sendKeys(Character.toString((char) ('A' + Math.random() * ('Z' - 'A' + 1))));
-								TimeUnit.SECONDS.sleep((int) (Math.random() * (5 - 2) + 2) / 100);
+								TimeUnit.SECONDS.sleep((int) (Math.random() * (5 - 2) + 2) / 60);
 							}
 
-							TimeUnit.MILLISECONDS.sleep(100);
+							TimeUnit.MILLISECONDS.sleep(300);
 
 							for (int i = 0; i < loops; i++) {
 								answerBox.sendKeys(Keys.BACK_SPACE);
@@ -107,11 +101,11 @@ public class Main {
 
 						} else {
 							answerBox.sendKeys(Character.toString(character));
-							TimeUnit.SECONDS.sleep((int) (Math.random() * (14 - 5) + 5) / 100);
+							TimeUnit.SECONDS.sleep((int) (Math.random() * (50 - 5) + 5) / 60);
 						}
 					}
 					answerBox.sendKeys(Keys.RETURN);
-					TimeUnit.MILLISECONDS.sleep(50);
+					TimeUnit.MILLISECONDS.sleep(100);
 
 				}
 			} catch (Exception e) {
